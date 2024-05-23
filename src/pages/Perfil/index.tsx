3b29abda6ef2787b31d2c334963ae2f7f7a3ec4d
JulioVieiraP <134452045+JulioVieiraP@users.noreply.github.com>
2024-05-23 13:59:32 -0300

    Style: adicionando a img da pizza

--- src/pages/Perfil/index.tsx
+++ src/pages/Perfil/index.tsx
@@ -1,64 +1,63 @@
 import ProductCard from '../../components/ProductCard'
 import Efoods from '../../model/Efood'
-import img1 from '../../assets/images/Restaurante1.png'
-import img2 from '../../assets/images/Restaurante2.png'
+import ImgPizza from '../../assets/images/Pizza.png'
 import Header from '../../components/Header'
 
 const Perfil = () => {
   const ListeRestaurantes: Efoods[] = [
     {
       id: 1,
-      title: 'Hioki Sushi',
+      title: 'Pizza Marguerita',
       description:
-        'Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida. Experimente o Japão sem sair do lar com nosso delivery!',
+        'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
       infos: ['Destaque da semana', 'Japonesa'],
       nota: '4.9',
-      image: img1
+      image: ImgPizza
     },
     {
       id: 2,
-      title: 'La Dolce Vita Trattoria',
+      title: 'Pizza Marguerita',
       description:
-        'A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida, pratos bem embalados e sabor inesquecível. Peça já!',
-      infos: ['Italiana'],
-      nota: '4.6',
-      image: img2
+        'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+      infos: ['Destaque da semana', 'Japonesa'],
+      nota: '4.9',
+      image: ImgPizza
     },
     {
       id: 3,
-      title: 'Hioki Sushi',
+      title: 'Pizza Marguerita',
       description:
-        'Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida. Experimente o Japão sem sair do lar com nosso delivery!',
+        'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
       infos: ['Destaque da semana', 'Japonesa'],
       nota: '4.9',
-      image: img1
+      image: ImgPizza
     },
     {
       id: 4,
-      title: 'La Dolce Vita Trattoria',
+      title: 'Pizza Marguerita',
       description:
-        'A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida, pratos bem embalados e sabor inesquecível. Peça já!',
-      infos: ['Italiana'],
-      nota: '4.6',
-      image: img2
+        'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+      infos: ['Destaque da semana', 'Japonesa'],
+      nota: '4.9',
+      image: ImgPizza
     },
     {
       id: 5,
-      title: 'Hioki Sushi',
+      title: 'Pizza Marguerita',
       description:
-        'Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida. Experimente o Japão sem sair do lar com nosso delivery!',
+        'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
       infos: ['Destaque da semana', 'Japonesa'],
       nota: '4.9',
-      image: img1
+      image: ImgPizza
     },
     {
       id: 6,
-      title: 'La Dolce Vita Trattoria',
+      title: 'Pizza Marguerita',
       description:
-        'A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida, pratos bem embalados e sabor inesquecível. Peça já!',
-      infos: ['Italiana'],
-      nota: '4.6',
-      image: img2
+        'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+      infos: ['Destaque da semana', 'Japonesa'],
+      nota: '4.9',
+      image: ImgPizza
     }
   ]
 
